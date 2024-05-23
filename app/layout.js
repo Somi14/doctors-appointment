8c58f82489f3e362acb796814e9840dfb5c0ebd2
somi <somigour2002@gmail.com>
2024-05-23 21:12:02 +0530

    Revert "first commit"
    This reverts commit c0e15a978ad098925c5a4260fa57a998898bff63.

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
-import { Outfit } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./_components/Header";
-import Footer from "./_components/Footer";
-import { Toaster } from 'sonner'
-//import { Toaster } from "@/components/ui/toaster";
-//it is layout.js which is visible to the viewers
-const outfit = Outfit({ subsets: ["latin"] });
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -15,15 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={outfit.className}>
-       <div className="md:px-20">
-        <Header/>
-        {children}
-        <Toaster />
-
-        </div> 
-        <Footer/>       
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode:false,
-    images:{
-        domains:['res.cloudinary.com', "lh3.googleusercontent.com"]
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
